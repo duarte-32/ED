495a37418c5e5e9b46edfdae1cb1669fe91ab02f
BrunoSousa001 <pv27437@alunos.estgv.ipv.pt>
2025-05-29 14:44:10 +0100

    Update main.c

--- main.c
+++ main.c
@@ -52,9 +52,10 @@ void listarDonoSubmenu(){
 
     do{
         printf("\n--- Submenu Listar Dono ---\n");
-        printf("| 1. Listar Donos                 |\n");
-        printf("| 2. Listar por ordem Alfabética  |\n");
-        printf("| 0. Voltar ao menu principal     |\n");
+        printf("| 1. Listar Donos                       |\n");
+        printf("| 2. Listar por ordem Alfabética        |\n");
+        printf("| 3. Listar por número de contribuinte  |\n");
+        printf("| 0. Voltar ao menu principal           |\n");
         printf("Escolha uma opção:\n");
         scanf("%d", &opcao);
 
@@ -66,6 +67,9 @@ void listarDonoSubmenu(){
             case 2:
                 listarCondutoresOrdemAlfa(donos);
                 break;
+            case 3:
+                listarCondutoresPorContribuinte(donos);
+                break;
             case 0:
                 printf("Voltar ao menu principal.\n");
                 break;
@@ -74,6 +78,28 @@ void listarDonoSubmenu(){
 
         }
     }while(opcao != 0);
+
+    return 0;
+}
+
+void listarVeiculoSubmenu(){
+    int opcao;
+    do {
+        printf("\n--- Submenu Listar Veículos ---\n");
+        printf("1 - Ordenar por Matrícula\n");
+        printf("2 - Ordenar por Marca\n");
+        printf("3 - Ordenar por Modelo\n");
+        printf("0 - Voltar\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &opcao);
+
+        if (opcao >= 1 && opcao <= 3) {
+            listarVeiculosOrdenados(veiculos, opcao);
+        } else if (opcao != 0) {
+            printf("Opção inválida.\n");
+        }
+
+    } while (opcao != 0);
 
     return 0;
 }
@@ -91,13 +117,14 @@ int main(){
 	do{
 		printf("******VIA VERMELHA******");
 		printf("\n-------------------------\n");
-		printf("| 1.Registar Dono           |\n");
-		printf("| 2.Listar Donos            |\n");
-		printf("| 3.Registar Veiculo        |\n");
-		printf("| 4.Listar Veiculos         |\n");
-		printf("| 5.Registar Passagem       |\n");
-		printf("| 6.Memoria Total Ocupada   |\n");
-		printf("| 0.Sair do Programa        |\n");
+		printf("| 1.Registar Dono               |\n");
+		printf("| 2.Listar Donos                |\n");
+		printf("| 3.Registar Veiculo            |\n");
+		printf("| 4.Listar Veiculos             |\n");
+		printf("| 5.Listar veículos ordenados   |\n");
+		printf("| 6.Registar Passagem           |\n");
+		printf("| 7.Memoria Total Ocupada       |\n");
+		printf("| 0.Sair do Programa            |\n");
 		printf("----------------------------\n");
 		scanf("%d",&op);
 		switch(op){
@@ -111,16 +138,19 @@ int main(){
 				registarVeiculos();
 				break;
 			case 4:
-				ListarVeiculos();
+                ListarVeiculos();
+                break;
+            case 5:
+				listarVeiculoSubmenu();
 				break;
-			case 5:
+			case 6:
 				registarPassagem();
 				break;
-            		case 6:{
-                		size_t memoriaTotal = calcularMemoriaTotal(donos, veiculos, sensores, distancias, passagens);
-                		printf("Memória total ocupada pelas estruturas de dados: %zu bytes\n", memoriaTotal);
-               			break;
-           		 }
+            case 7:{
+                size_t memoriaTotal = calcularMemoriaTotal(donos, veiculos, sensores, distancias, passagens);
+                printf("Memória total ocupada pelas estruturas de dados: %zu bytes\n", memoriaTotal);
+                break;
+            }
 			case 0:
 				break;
 			default:
