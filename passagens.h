598b849980e9473eabb8a5b91e770b0ebf0e7a8d
BrunoSousa001 <pv27437@alunos.estgv.ipv.pt>
2025-06-01 18:28:01 +0100

    Update passagens.h

--- passagens.h
+++ passagens.h
@@ -11,5 +11,16 @@ typedef struct Passagem {
 
 void CarregarPassagens(const char* arquivo);
 void registarPassagem();
+time_t stringParaTempo(const char* data);
+void rankingCirculacao(Passagem* passagens, Distancia* distancias, Veiculo* veiculos, const char* dataInicio, const char* dataFim);
+void rankingPorMarca(Passagem* passagens, Distancia* distancias, Veiculo* veiculos, const char* dataInicio, const char* dataFim);
+void listarInfracoes(Passagem* passagens, Distancia* distancias, Veiculo* veiculos, const char* dataInicio, const char* dataFim);
+void rankingInfracoes(Passagem* passagens, Distancia* distancias, Veiculo* veiculos, const char* dataInicio, const char* dataFim);
+void calcularVelocidadesMedias(Passagem* passagens, Distancia* distancias, Veiculo* veiculos);
+void marcaMaisRapida(Passagem* passagens, Distancia* distancias, Veiculo* veiculos);
+void donoMaisRapido(Passagem* passagens, Distancia* distancias, Veiculo* veiculos, Dono* donos);
+void mediaPorCodigoPostal(Passagem* passagens, Distancia* distancias, Veiculo* veiculos, Dono* donos, const char* codigoPostal);
+void marcaMaisComum(Veiculo* veiculos);
+
 
 #endif
