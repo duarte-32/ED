84e5f896e3a49fd4e755a0d4f4339d476c4d6999
BrunoSousa001 <pv27437@alunos.estgv.ipv.pt>
2025-06-01 18:24:27 +0100

    Update main.c
    main alterado e completo

--- main.c
+++ main.c
@@ -95,26 +95,123 @@ void listarVeiculoSubmenu(){
     int opcao;
     do {
         printf("\n--- Submenu Listar Veículos ---\n");
-        printf("1 - Ordenar por Matrícula\n");
-        printf("2 - Ordenar por Marca\n");
-        printf("3 - Ordenar por Modelo\n");
-        printf("4 - Pesquisar por Matricula\n");
-        printf("0 - Voltar\n");
+        printf("|   1.Ordenar por Matrícula     |\n");
+        printf("|   2.Ordenar por Marca         |\n");
+        printf("|   3.Ordenar por Modelo        |\n");
+        printf("|   0.Voltar                    |\n");
         printf("Escolha uma opção: ");
         scanf("%d", &opcao);
 
         if (opcao >= 1 && opcao <= 3) {
             listarVeiculosOrdenados(veiculos, opcao);
-        } else if (opcao == 4){
-            pesquisarMatricula();
-        }
-        else if (opcao != 0) {
+        } else if (opcao != 0) {
             printf("Opção inválida.\n");
         }
 
     } while (opcao != 0);
 
-    return;
+    return 0;
+}
+
+void subMenuAnaliseCirculacao(){
+    char dataInicio[30], dataFim[30];
+    int opcao;
+
+    getchar(); //Limpar \n pendente do scnaf anterior
+
+    printf("\n--- Submenu Análise de Circulação --\n");
+    printf("Introduza o período a analisar: \n");
+
+    printf("Data início (dd-mm-yyyy hh:mm:ss: ");
+    fgets(dataInicio, sizeof(dataInicio), stdin);
+    dataInicio[strcspn(dataInicio, "\n")] = '\0';
+
+    printf("Data fim (dd-mm-yyyy hh:mm:ss: ");
+    fgets(dataFim, sizeof(dataFim), stdin);
+    dataFim[strcspn(dataFim, "\n")] = '\0';
+
+     do {
+        printf("\n--- Análises Disponíveis ---\n");
+        printf("| 1.Ranking de circulação por veículo (total km)    |\n");
+        printf("| 2.Ranking por marca (total km)                    |\n");
+        printf("| 3.Veículos com velocidade média > 120 km/h        |\n");
+        printf("| 4.Ranking de infrações por veículo                |\n");
+        printf("| 0.Voltar                                          |\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &opcao);
+
+        switch (opcao) {
+            case 1:
+                rankingCirculacao(passagens, distancias, veiculos, dataInicio, dataFim);
+                break;
+            case 2:
+                rankingPorMarca(passagens, distancias, veiculos, dataInicio, dataFim);
+                break;
+            case 3:
+                listarInfracoes(passagens, distancias, veiculos, dataInicio, dataFim);
+                break;
+            case 4:
+                rankingInfracoes(passagens, distancias, veiculos, dataInicio, dataFim);
+                break;
+            case 0:
+                printf("A voltar ao menu principal...\n");
+                break;
+            default:
+                printf("Opção inválida.\n");
+        }
+    } while (opcao != 0);
+}
+
+void submenuAnalisesAvancadas() {
+    int opcao;
+    char codPostal[20];
+
+    do {
+        printf("\n===== Submenu Análises Avançadas =====\n");
+        printf("12 - Listar velocidades médias por veículo\n");
+        printf("13 - Qual a marca mais rápida\n");
+        printf("14 - Qual o dono mais rápido\n");
+        printf("15 - Velocidade média por código postal\n");
+        printf("16 - Marca mais comum\n");
+        printf("17 - Exportar base de dados para CSV\n");
+        printf("18 - Exportar base de dados para XML\n");
+        printf("0  - Voltar ao menu principal\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &opcao);
+        getchar(); // limpar \n
+
+        switch (opcao) {
+            case 12:
+                calcularVelocidadesMedias(passagens, distancias, veiculos);
+                break;
+            case 13:
+                marcaMaisRapida(passagens, distancias, veiculos);
+                break;
+            case 14:
+                donoMaisRapido(passagens, distancias, veiculos, donos);
+                break;
+            case 15:
+                printf("Introduza o código postal: ");
+                fgets(codPostal, sizeof(codPostal), stdin);
+                codPostal[strcspn(codPostal, "\n")] = '\0';
+                mediaPorCodigoPostal(passagens, distancias, veiculos, donos, codPostal);
+                break;
+            case 16:
+                marcaMaisComum(veiculos);
+                break;
+            case 17:
+                exportarParaCSV(donos, veiculos, sensores, passagens);
+                break;
+            case 18:
+                exportarParaXML(donos, veiculos);
+                break;
+            case 0:
+                printf("A voltar ao menu principal...\n");
+                break;
+            default:
+                printf("Opção inválida. Tente novamente.\n");
+        }
+    } while (opcao != 0);
 }
 
 
@@ -136,7 +233,9 @@ int main(){
 		printf("| 4.Listar Veiculos             |\n");
 		printf("| 5.Listar veículos ordenados   |\n");
 		printf("| 6.Registar Passagem           |\n");
-		printf("| 7.Memoria Total Ocupada       |\n");
+		printf("| 7.Análise de Circulação       |\n");
+		printf("| 8. Análises Avançadas         |\n");
+		printf("| 9.Memoria Total Ocupada       |\n");
 		printf("| 0.Sair do Programa            |\n");
 		printf("----------------------------\n");
 		scanf("%d",&op);
@@ -159,9 +258,15 @@ int main(){
 			case 6:
 				registarPassagem();
 				break;
-            case 7:{
+            case 7:
+                subMenuAnaliseCirculacao();
+                break;
+            case 8:
+                submenuAnalisesAvancadas();
+                break;
+            case 9:{
                 size_t memoriaTotal = calcularMemoriaTotal(donos, veiculos, sensores, distancias, passagens);
-                printf("Memória total ocupada pelas estruturas de dados: %zu bytes = %0.2f MB\n", memoriaTotal,(float)memoriaTotal/1000000);
+                printf("Memória total ocupada pelas estruturas de dados: %zu bytes\n", memoriaTotal);
                 break;
             }
 			case 0:
@@ -173,6 +278,6 @@ int main(){
 	}while(op !=0);
 
 	printf("Programa fechado. \n");
-
 	return 0;
 }
+
