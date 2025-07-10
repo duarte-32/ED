bde0387a101b497095a722054ed9efb003ee009f
BrunoSousa001 <pv27437@alunos.estgv.ipv.pt>
2025-07-10 15:48:50 +0100

    Update uteis.c

--- uteis.c
+++ uteis.c
@@ -30,43 +30,6 @@ int compararPorModelo(const void* a, const void* b) {
     return strcmp((*(Veiculo**)a)->modelo, (*(Veiculo**)b)->modelo);
 }
 
-TempoComMs stringParaTempoComMs(const char* data) {
-    struct tm tm = {0};
-    int dia, mes, ano, hora, minuto, segundo, milissegundos = 0;
-    TempoComMs resultado;
-
-    int lidos =  sscanf(data, "%d-%d-%d %d:%d:%d.%d", &dia, &mes, &ano, &hora, &minuto, &segundo, &milissegundos);
-
-    if (lidos < 6) {
-        printf("Erro: Formato de data inválido: %s\n", data);
-        resultado.tempoBase = (time_t)-1;
-        resultado.milissegundos = -1;
-        return resultado;
-    }
-
-    // Validação simples dos valores
-    if (dia < 1 || dia > 31 || mes < 1 || mes > 12 || hora < 0 || hora > 23 ||
-        minuto < 0 || minuto > 59 || segundo < 0 || segundo > 59 || milissegundos < 0
-        || milissegundos > 999) {
-        fprintf(stderr, "Erro: Valores de data/hora fora do intervalo: %s\n", data);
-        resultado.tempoBase = (time_t)-1;
-        resultado.milissegundos = -1;
-        return resultado;
-    }
-
-    tm.tm_mday = dia;
-    tm.tm_mon = mes - 1;
-    tm.tm_year = ano - 1900;
-    tm.tm_hour = hora;
-    tm.tm_min = minuto;
-    tm.tm_sec = segundo;
-
-    resultado.milissegundos = milissegundos;
-    resultado.tempoBase = mktime(&tm);
-
-    return resultado;
-}
-
 
 time_t stringParaTempo(const char* data){
     struct tm tm;
@@ -78,6 +41,13 @@ time_t stringParaTempo(const char* data){
         printf("Erro: formato de data inválido -> %s\n", data);
         return (time_t)-1;
     }
+
+    // Verificações de validade
+    if (ano < 1900 || mes < 1 || mes > 12 || dia < 1 || dia > 31 || hora < 0 || hora > 23 || minuto < 0 || minuto > 59 || segundo < 0 || segundo > 59) {
+        printf("Erro: valores de data ou hora fora do intervalo -> %s\n", data);
+        return (time_t)-1;
+    }
+
 
     tm.tm_mday = dia;
     tm.tm_mon = mes - 1;// Em C, janeiro = 0
